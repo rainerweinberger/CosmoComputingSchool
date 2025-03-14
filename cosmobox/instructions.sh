15b22b64e31780a47d6174562b356757d45b2edb
rainerweinberger <50906527+rainerweinberger@users.noreply.github.com>
2025-03-14 11:31:26 +0100

    Update instructions.sh

--- cosmobox/instructions.sh
+++ cosmobox/instructions.sh
@@ -2,13 +2,13 @@ rm -rf ./cosmo_box_sim
 
 mkdir ./cosmo_box_sim
 cd ./cosmo_box_sim
-git clone git@bitbucket.org:RWeinberger/rw_arepo.git arepo #clone https://gitlab.mpcdf.mpg.de/vrs/arepo.git
+git clone https://github.com/rainerweinberger/arepo.git arepo #clone https://gitlab.mpcdf.mpg.de/vrs/arepo.git
 cp ../CosmoComputingSchool/cosmobox/ics.hdf5 ./
 cp ../CosmoComputingSchool/cosmobox/param.txt ./
 cp ../CosmoComputingSchool/cosmobox/output_list.txt ./
 cp ../CosmoComputingSchool/cosmobox/Config.sh ./arepo/
 cd arepo
-export SYSTYPE="macOShomebrew"
+export SYSTYPE="Newtonn21"
 echo $SYSTYPE
 make
 cd ..
